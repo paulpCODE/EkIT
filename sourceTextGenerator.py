d49fdcfd67568d4ccdba6a44c908adb0525033fa
Dunkeshon <andrei.ioffe@gmail.com>
2022-02-13 22:30:40 +0200

    python script - cource code maker done

---
+++ sourceTextGenerator.py
@@ -0,0 +1,33 @@
+import os
+
+
+def FlushToFile(fromFileName, resultFileName = 'SourceCodeFile.txt'):
+    try:
+        with open(fromFileName, "r") as fromFile:
+            fileText = str("---------------" + fromFileName + "--------------- \n") + fromFile.read()
+            with open(resultFileName, "a") as toFile:
+                toFile.write('\n'+fileText)
+    except FileNotFoundError:
+        return 0
+
+resultFileName = 'SourceCodeFile.txt'
+with open(resultFileName, "w") as myfile:
+    myfile.truncate()
+
+thisdir = os.getcwd()
+for r, d, f in os.walk(thisdir):
+    for file in f:
+        tempFileBegin = ''
+        if file.endswith(".h"):
+            tempFileBegin = file[:-2]
+            cppFileName = tempFileBegin + ".cpp"
+            FlushToFile(file)
+            FlushToFile(cppFileName)
+
+
+
+
+
+
+
+allWords = list()
